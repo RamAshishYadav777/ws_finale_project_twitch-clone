2f68b83b384202ecbf1dba8d862cb98c7ffc19ce
Ram Ashish Yadav <ramashishyadav777@gmail.com>
2025-12-07 10:53:20 +0530

    commit3

--- components/stream-player/header.tsx
+++ components/stream-player/header.tsx
@@ -5,7 +5,7 @@ import {
   useParticipants,
   useRemoteParticipant,
 } from "@livekit/components-react";
-import { UserIcon } from "lucide-react";
+import { User } from "lucide-react";
 
 import { UserAvatar, UserAvatarSkeleton } from "@/components/user-avatar";
 import { VerifiedMark } from "@/components/verified-mark";
@@ -55,7 +55,7 @@ export function Header({
           <p className="text-sm font-semibold">{name}</p>
           {isLive ? (
             <div className="font-semibold flex gap-x-1 items-center text-xs text-rose-500">
-              <UserIcon className="h-4 w-4" />
+              <User className="h-4 w-4" />
               <p>
                 {participantCount}{" "}
                 {participantCount === 1 ? "viewer" : "viewers"}
